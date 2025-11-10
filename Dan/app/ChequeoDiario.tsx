3d704cc491e48cc10b0f30449713bdf5e8c18d2e
lucotuco <lucasschlez@gmail.com>
2025-11-10 01:33:30 -0300

    avances chequeos

--- Dan/app/ChequeoDiario.tsx
+++ Dan/app/ChequeoDiario.tsx
@@ -1,28 +1,25 @@
 import { StyleSheet, ScrollView } from 'react-native';
-import { Button } from 'react-native-paper';
-import React from 'react';
 import { Text, View } from '@/components/Themed';
 import MedioLogo from '@/components/MedioLogo';
 import Slider from '@react-native-community/slider';
 import { useState } from 'react';
 import Card from '@/components/Card';
-import { Color } from 'react-native/types_generated/Libraries/Animated/AnimatedExports';
 
 export default function CheckupsScreen() {
-  const [sliderValues, setSliderValues] = useState([0, 0, 0, 0, 0]);
+  const [sliderValues, setSliderValues] = useState([0, 0, 0, 0, 0, 0]);
 
   const handleSliderChange = (value: number, index: number) => {
     const newValues = [...sliderValues];
     newValues[index] = value;
     setSliderValues(newValues);
+    console.log('Slider ',index ,':', newValues[index]);
   };
 
   return (
     <ScrollView style={[styles.container, {'backgroundColor': '#fff'},]} contentContainerStyle={styles.content}>
          <MedioLogo/>
-         <Text style={{fontSize:30, fontWeight:'700', color:'#1d1564', marginBottom:10, alignItems: 'center', paddingLeft:50}}>Chequeo Diario</Text>
           <Card>
-             <Text style={styles.sliderLabel}>Energía :</Text>
+             <Text style={styles.sliderLabel}>Energia:</Text>
              <Slider 
                style={{width: 300, height: 40}}
                minimumValue={0}
@@ -36,7 +33,7 @@ export default function CheckupsScreen() {
                onValueChange={(value) => handleSliderChange(value, 0)}
              />
 
-             <Text style={styles.sliderLabel}>Motivación :</Text>
+             <Text style={styles.sliderLabel}>Energia:</Text>
              <Slider 
                style={{width: 300, height: 40}}
                minimumValue={0}
@@ -50,7 +47,7 @@ export default function CheckupsScreen() {
                onValueChange={(value) => handleSliderChange(value, 1)}
              />
 
-             <Text style={styles.sliderLabel}>Estado Emocional:</Text>
+             <Text style={styles.sliderLabel}>Energia:</Text>
              <Slider 
                style={{width: 300, height: 40}}
                minimumValue={0}
@@ -64,7 +61,7 @@ export default function CheckupsScreen() {
                onValueChange={(value) => handleSliderChange(value, 2)}
              />
 
-             <Text style={styles.sliderLabel}>Sueño :</Text>
+             <Text style={styles.sliderLabel}>Energia:</Text>
              <Slider 
                style={{width: 300, height: 40}}
                minimumValue={0}
@@ -77,7 +74,7 @@ export default function CheckupsScreen() {
                testID='1'
                onValueChange={(value) => handleSliderChange(value, 3)}
              />
-             <Text style={styles.sliderLabel}>Dolor o Molestia:</Text>
+             <Text style={styles.sliderLabel}>Energia:</Text>
              <Slider 
                style={{width: 300, height: 40}}
                minimumValue={0}
@@ -90,11 +87,21 @@ export default function CheckupsScreen() {
                testID='1'
                onValueChange={(value) => handleSliderChange(value, 4)}
              />
-         </Card>
 
-          <Button mode="contained" style={{marginTop:20, backgroundColor:'#1d1564ff'}} onPress={() => console.log('Guardar Chequeo ', sliderValues[0], sliderValues[1], sliderValues[2], sliderValues[3], sliderValues[4])}>
-            <Text style={{color:'#ffffffff',fontSize:20, fontWeight:'600'}}>Guardar Chequeo</Text>
-          </Button>
+             <Text style={styles.sliderLabel}>Energia:</Text>
+             <Slider 
+               style={{width: 300, height: 40}}
+               minimumValue={0}
+               maximumValue={10}
+               minimumTrackTintColor="#1d1564ff"
+               maximumTrackTintColor="#949494ff"
+               step={1}
+               tapToSeek={true}
+               thumbTintColor='#1d1564ff'
+               testID='1'
+               onValueChange={(value) => handleSliderChange(value, 5)}
+             />
+         </Card>
           
     </ScrollView>
   );
