bd6abaa262334b8355ed2592f932dfbdef7a9fb7
Lucas Martin Schlez <lucasschlez@gmail.com>
2025-11-13 11:43:02 -0300

    Revert "Refine audio recorder playback handling and header styling"
    This reverts commit 6fcbf801f996e4e548a778acd264b053e29b8608.

--- Dan/app/(tabs)/_layout.tsx
+++ Dan/app/(tabs)/_layout.tsx
@@ -35,17 +35,14 @@ export default function TabLayout() {
         },
         headerTitle: () => <HeaderLogo />,
         headerTitleAlign: 'center',
+        
         headerTitleContainerStyle: {
           width: '200%', height:'140%'
         },
         headerStyle: {
           backgroundColor: theme.background,
-          shadowColor: 'transparent',
-          elevation: 0,
-          borderBottomWidth: 0,
         },
         headerTintColor: theme.text,
-        headerShadowVisible: false,
         headerShown: useClientOnlyValue(false, true),
       }}>
       <Tabs.Screen
